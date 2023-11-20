9b5f68f777eaf910e0e13240f593567d8adbfb99
Mortikiy <114204417+Mortikiy@users.noreply.github.com>
2023-11-20 18:31:50 -0500

    Update lkmasg1_common.h

--- lkmasg1_common.h
+++ lkmasg1_common.h
@@ -1,19 +1,28 @@
 /**
+
  * This is a header file containing common dependencies &
+
  * resources that both kernel module files will access
+
  */
 
 #ifndef LKMASG1_COMMON_H
 #define LKMASG1_COMMON_H
-
+#include <linux/mutex.h>
 #define BUFFER_SIZE 1024
 #define CLASS_NAME "char"
 
-
 // Circular buffer to store data in FIFO fashion
-// Declared with extern to keep open to both modules
-extern char mainBuffer[BUFFER_SIZE];
-extern int buffer_head;
-extern int buffer_tail;
 
+// Declared with extern to keep open to both modules
+
+extern char mainBuffer[BUFFER_SIZE];
+
+extern int buffer_head;
+
+extern int buffer_tail;
+// Mutex to guard access to shared data
+
+extern struct mutex buffer_mutex;
 #endif
+
