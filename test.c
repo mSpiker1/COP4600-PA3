24a3cecbc9f77248d4fe83f0981fb81a10292887
Mortikiy <114204417+Mortikiy@users.noreply.github.com>
2023-11-20 18:32:45 -0500

    Update test.c

--- test.c
+++ test.c
@@ -1,56 +1,64 @@
-/**
-NOTE: This test file does not currently work as intended for our use case
-This is still the original test file from the second PA
- */
 #include <stdio.h>
+
 #include <stdlib.h>
-#include <errno.h>
+
 #include <fcntl.h>
+
 #include <string.h>
+
 #include <unistd.h>
 
-#define BUFFER_LENGTH 256           ///< The buffer length (crude but fine)
-static char receive[BUFFER_LENGTH]; ///< The receive buffer from the LKM
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        printf("Usage: test <path to device>\n");
-        exit(0);
-    }
-    char *devicepath = argv[1];
 
-    int ret, fd;
-    char stringToSend[BUFFER_LENGTH];
-    printf("Starting device test code example...\n");
-    fd = open(devicepath, O_RDWR); // Open the device with read/write access
-    if (fd < 0)
-    {
+#define DEVICE_PATH "/dev/lkmasg1_input"
+
+
+
+int main() {
+
+    int fd;
+
+    char buf[256];
+
+
+
+    fd = open(DEVICE_PATH, O_RDONLY);
+
+    if (fd < 0) {
+
         perror("Failed to open the device...");
-        return errno;
-    }
-    printf("Type in a short string to send to the kernel module:\n");
-    scanf("%[^\n]%*c", stringToSend); // Read in a string (with spaces)
-    printf("Writing message to the device [%s].\n", stringToSend);
-    ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-    if (ret < 0)
-    {
-        perror("Failed to write the message to the device.");
-        return errno;
+
+        return -1;
+
     }
 
-    printf("Press ENTER to read back from the device...\n");
-    getchar();
 
-    printf("Reading from the device...\n");
-    ret = read(fd, receive, BUFFER_LENGTH); // Read the response from the LKM
-    if (ret < 0)
-    {
-        perror("Failed to read the message from the device.");
-        return errno;
+
+    memset(buf, 0, sizeof(buf));
+
+    ssize_t bytesRead = read(fd, buf, sizeof(buf) - 1);
+
+
+
+    if (bytesRead < 0) {
+
+        perror("Failed to read from the device...");
+
+        close(fd);
+
+        return -1;
+
     }
-    printf("The received message is: [%s]\n", receive);
-    printf("End of the program\n");
+
+
+
+    printf("Read from the device: %s\n", buf);
+
+
+
+    close(fd);
+
     return 0;
+
 }
+
