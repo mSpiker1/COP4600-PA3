d6e82a0ceb68918c671d035023dfb1f823f95a01
Mortikiy <114204417+Mortikiy@users.noreply.github.com>
2023-11-21 18:23:22 -0500

    shared_memory.h

--- lkmasg1_common.h
+++ lkmasg1_common.h
@@ -1,28 +1,20 @@
-/**
+#ifndef SHARED_MEMORY_H
 
- * This is a header file containing common dependencies &
+#define SHARED_MEMORY_H
 
- * resources that both kernel module files will access
 
- */
 
-#ifndef LKMASG1_COMMON_H
-#define LKMASG1_COMMON_H
-#include <linux/mutex.h>
-#define BUFFER_SIZE 1024
-#define CLASS_NAME "char"
+#include <linux/types.h>
 
-// Circular buffer to store data in FIFO fashion
 
-// Declared with extern to keep open to both modules
 
-extern char mainBuffer[BUFFER_SIZE];
+#define SHARED_MEM_SIZE 1042
 
-extern int buffer_head;
 
-extern int buffer_tail;
-// Mutex to guard access to shared data
 
-extern struct mutex buffer_mutex;
+extern char shared_memory[SHARED_MEM_SIZE];
+
+
+
 #endif
 
